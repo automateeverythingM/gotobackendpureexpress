3b3a1a8cc37d10142ab67bedb15563b62f0571a7
automateeverythingM <markospavicbg@gmail.com>
2021-03-24 09:45:31 +0100

    working events  laeve, user join, typing, stop abstraction on create events

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,6 @@
+const { nanoid } = require("nanoid");
+const { MESSAGE_TYPE } = require("./actions");
+
 /**
  *  Receiving event and pass data to all user in same room except emitter
  *
@@ -6,12 +9,31 @@
  * @param {string} emitEvent
  * @param {string} roomName
  */
-exports.socketOnReceiveEmit = (socket, onEvent, emitEvent, roomName) => {
-  console.log(
-    "🚀 ~ file: index.js ~ line 2 ~ socketOnReceiveEmit ~ onEvent",
-    onEvent
-  );
-  socket.to(roomName).on(onEvent, (...args) => {
-    socket.to(roomName).emit(emitEvent, ...args);
+exports.socketOnReceiveEmit = (socket, onEvent, emitEvent, func) => {
+  socket.on(onEvent, ({ roomName, ...rest }) => {
+    if (func) func(roomName, rest);
+    if (emitEvent) socket.to(roomName).emit(emitEvent, rest);
   });
+};
+
+exports.messageGenerator = (type, data) => {
+  switch (type) {
+    case MESSAGE_TYPE.USER_JOIN:
+      return {
+        type: { name: "SERVER", subType: "JOIN" },
+        content: `${data.displayName} joined room.`,
+        uid: nanoid(),
+        user: data,
+      };
+    case MESSAGE_TYPE.USER_LEFT:
+      return {
+        type: { name: "SERVER", subType: "LEFT" },
+        content: `${data.displayName} left room.`,
+        uid: nanoid(),
+        user: data,
+      };
+
+    default:
+      break;
+  }
 };
